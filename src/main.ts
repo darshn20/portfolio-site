a9c1cb3162faf189f9508038a106350ebadd3a80
darshn20 <darshanbhuva57@gmail.com>
2025-08-25 16:17:34 +0530

    Add vercel analytics and speed-insights

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 
+inject();
+injectSpeedInsights();
 bootstrapApplication(AppComponent, {
   providers: [provideClientHydration(), provideHttpClient(withFetch())]
 });
